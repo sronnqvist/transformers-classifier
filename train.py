7df4a685f2b5b3060b93f2801f96437f14ddf336
Samuel Rönnqvist <saanro@utu.fi>
2020-11-06 15:59:09 +0200

    adapted to registers task

--- train.py
+++ train.py
@@ -2,7 +2,13 @@
 
 import sys
 import math
+import numpy as np
+from os.path import isfile
+import csv
 
+from scipy.sparse import lil_matrix
+
+from sklearn.metrics import precision_recall_fscore_support
 from sklearn.preprocessing import MultiLabelBinarizer
 
 from tensorflow.keras import Model
@@ -10,10 +16,11 @@ from tensorflow.keras.layers import Input, Dropout, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import CategoricalCrossentropy, BinaryCrossentropy
 from tensorflow.keras.metrics import CategoricalAccuracy, AUC
+from tensorflow.keras.callbacks import Callback
 
 from tensorflow_addons.metrics import F1Score
 
-from transformers import AutoConfig, AutoTokenizer, TFAutoModel
+from transformers import AutoConfig, AutoTokenizer, TFAutoModel, TFAutoModelForSequenceClassification
 from transformers.optimization_tf import create_optimizer
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
@@ -25,7 +32,7 @@ from common import timed
 
 # Parameter defaults
 DEFAULT_BATCH_SIZE = 8
-DEFAULT_SEQ_LEN = 128
+DEFAULT_SEQ_LEN = 512
 DEFAULT_LR = 5e-5
 DEFAULT_WARMUP_PROPORTION = 0.1
 
@@ -43,7 +50,7 @@ def argparser():
                     help='batch size for training')
     ap.add_argument('--epochs', metavar='INT', type=int, default=1,
                     help='number of training epochs')
-    ap.add_argument('--lr', '--learning_rate', metavar='FLOAT', type=float, 
+    ap.add_argument('--lr', '--learning_rate', metavar='FLOAT', type=float,
                     default=DEFAULT_LR, help='learning rate')
     ap.add_argument('--seq_len', metavar='INT', type=int,
                     default=DEFAULT_SEQ_LEN,
@@ -56,6 +63,10 @@ def argparser():
                     help='input file format')
     ap.add_argument('--multiclass', default=False, action='store_true',
                     help='task has exactly one label per text')
+    ap.add_argument('--output_file', default=None, metavar='FILE',
+                    help='save model to file')
+    ap.add_argument('--log_file', default="train.log", metavar='FILE',
+                    help='log parameters and performance to file')
     return ap
 
 
@@ -63,8 +74,10 @@ def argparser():
 def load_pretrained(options):
     name = options.model_name
     config = AutoConfig.from_pretrained(name)
+    config.return_dict = True
     tokenizer = AutoTokenizer.from_pretrained(name, config=config)
     model = TFAutoModel.from_pretrained(name, config=config)
+    #model = TFAutoModelForSequenceClassification.from_pretrained(name, config=config)
 
     if options.seq_len > config.max_position_embeddings:
         warning(f'--seq_len ({options.seq_len}) > max_position_embeddings '
@@ -106,7 +119,8 @@ def build_classifier(pretrained_model, num_labels, optimizer, options):
     inputs = [input_ids, attention_mask]
 
     pretrained_outputs = pretrained_model(inputs)
-    pooled_output = pretrained_outputs[1]
+    #pooled_output = pretrained_outputs[1]
+    pooled_output = pretrained_outputs['last_hidden_state'][:,0,:] #CLS
 
     # TODO consider Dropout here
     if options.multiclass:
@@ -117,11 +131,12 @@ def build_classifier(pretrained_model, num_labels, optimizer, options):
         output = Dense(num_labels, activation='sigmoid')(pooled_output)
         loss = BinaryCrossentropy()
         metrics = [
-            F1Score(name='f1', num_classes=num_labels, average='micro',
-                    threshold=0.5),
+            #F1Score(name='f1_th0.3', num_classes=num_labels, average='micro', threshold=0.3),
+            F1Score(name='f1_th0.4', num_classes=num_labels, average='micro', threshold=0.4),
+            #F1Score(name='f1_th0.5', num_classes=num_labels, average='micro', threshold=0.5),
             AUC(name='auc', multi_label=True)
         ]
-        
+    #output = pretrained_outputs # test
     model = Model(
         inputs=inputs,
         outputs=[output]
@@ -154,6 +169,7 @@ def load_data(fn, options):
 
 def make_tokenization_function(tokenizer, options):
     seq_len = options.seq_len
+    @timed
     def tokenize(text):
         tokenized = tokenizer(
             text,
@@ -180,13 +196,80 @@ def prepare_classifier(num_train_examples, num_labels, options):
     return model, tokenizer, optimizer
 
 
+def optimize_threshold(model, train_X, train_Y, test_X, test_Y, epoch=None):
+    labels_prob = model.predict(train_X, verbose=1)
+
+    best_f1 = 0.
+    print("Optimizing threshold...\nThres.\tF1\tPrec.\tRecall")
+    for threshold in np.arange(0.1, 0.9, 0.05):
+        labels_pred = lil_matrix(labels_prob.shape, dtype='b')
+        labels_pred[labels_prob>=threshold] = 1
+        precision, recall, f1, _ = precision_recall_fscore_support(train_Y, labels_pred, average="micro")
+        print("%.2f\t%.4f\t%.4f\t%.4f" % (threshold, precision, recall, f1), end="")
+        if f1 > best_f1:
+            print("\t*")
+            best_f1 = f1
+            #best_f1_epoch = epoch
+            best_f1_threshold = threshold
+        else:
+            print()
+
+    #print("Current F_max:", best_f1, "epoch", best_f1_epoch+1, "threshold", best_f1_threshold, '\n')
+    #print("Current F_max:", best_f1, "threshold", best_f1_threshold, '\n')
+
+    test_labels_prob = model.predict(test_X, verbose=1)
+    test_labels_pred = lil_matrix(test_labels_prob.shape, dtype='b')
+    test_labels_pred[test_labels_prob>=best_f1_threshold] = 1
+    test_precision, test_recall, test_f1, _ = precision_recall_fscore_support(test_Y, test_labels_pred, average="micro")
+    if epoch:
+        epoch_str = ", epoch %d" % epoch
+    else:
+        epoch_str = ""
+    print("\nValidation/Test performance at threshold %.2f%s: Prec. %.4f, Recall %.4f, F1 %.4f" % (best_f1_threshold, epoch_str, test_precision, test_recall, test_f1))
+    return test_f1
+
+
+class Logger:
+    def __init__(self, filename, model, params):
+        self.filename = filename
+        self.model = model
+        self.log = dict([('p%s'%p, v) for p, v in params.items()])
+
+    def record(self, epoch, logs):
+        for k in logs:
+            self.log['_%s' % k] = logs[k]
+        self.log['_Epoch'] = epoch
+        self.write()
+
+    def write(self):
+        file_exists = isfile(self.filename)
+        with open(self.filename, 'a') as csvfile:
+            writer = csv.DictWriter(csvfile, sorted(self.log.keys()))
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow(self.log)
+
+
+class EvalCallback(Callback):
+    def __init__(self, model, train_X, train_Y, test_X, test_Y, logfile="train.log", params={}):
+        self.model = model
+        self.train_X = train_X
+        self.train_Y = train_Y
+        self.test_X = test_X
+        self.test_Y = test_Y
+        self.logger = Logger(logfile, self.model, params)
+    def on_epoch_end(self, epoch, logs={}):
+        logs['f1'] = optimize_threshold(self.model, self.train_X, self.train_Y, self.test_X, self.test_Y, epoch=epoch)
+        self.logger.record(epoch, logs)
+
+
 def main(argv):
     options = argparser().parse_args(argv[1:])
 
     train_texts, train_labels = load_data(options.train, options)
     dev_texts, dev_labels = load_data(options.dev, options)
     num_train_examples = len(train_texts)
-    
+
     label_encoder = MultiLabelBinarizer()
     label_encoder.fit(train_labels)
     train_Y = label_encoder.transform(train_labels)
@@ -203,13 +286,25 @@ def main(argv):
     train_X = tokenize(train_texts)
     dev_X = tokenize(dev_texts)
 
+    eval_callback = EvalCallback(classifier, train_X, train_Y, dev_X, dev_Y,
+                                    logfile=options.log_file,
+                                    params={'LR': options.lr, 'N_epochs': options.epochs, 'BS': options.batch_size})
     history = classifier.fit(
         train_X,
         train_Y,
         epochs=options.epochs,
         batch_size=options.batch_size,
         validation_data=(dev_X, dev_Y),
+        callbacks=[eval_callback]
     )
+    #print(classifier.predict(dev_X))
+    #optimize_threshold(classifier, train_X, train_Y, dev_X, dev_Y)
+    try:
+        if options.output_file:
+            print("Saving model to %s" % options.output_file)
+            classifier.save_weights(options.output_file)
+    except:
+        pass
 
     return 0
 
