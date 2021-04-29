9d7e775f7222fe32519804015a7678b5f557ce5e
Samuel Rönnqvist <saanro@utu.fi>
2021-04-29 13:14:30 +0300

    swapped files

--- slurm_load.sh
+++ slurm_load.sh
@@ -22,23 +22,22 @@ module purge
 module load tensorflow/2.2-hvd
 source transformers3.4/bin/activate
 
-
-
 export PYTHONPATH=/scratch/project_2002026/samuel/transformer-text-classifier/transformers3.4/lib/python3.7/site-packages:$PYTHONPATH
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-#export MODEL_DIR=/scratch/project_2002026/bert/cased_L-12_H-768_A-12
-export MODEL_DIR=/scratch/project_2002026/bert/cased_L-24_H-1024_A-16
 
-export SOURCE_DIR=/scratch/project_2002026/multilabel_bert/neuro_classifier/multilabel
+SRC=fr
+TRG=$1
+BG="en fi sv"
+export BG_FILES="data/eacl/en/train.tsv data/eacl/fi/train.tsv data/eacl/sv/train.tsv"
 
-SRC=$1
-TRG=$2
 export TRAIN_DIR=data/eacl/$SRC
 export DEV_DIR=data/eacl/$TRG
+#export TEST_FILES="data/eacl/en/test.tsv;data/eacl/fi/test.tsv;data/eacl/fr/test.tsv;data/eacl/sv/test.tsv"
+
 export OUTPUT_DIR=output
 
 mkdir -p "$OUTPUT_DIR"
 
+MODEL_ALIAS="xlmR"
 #MODEL="jplu/tf-camembert-base"
 #MODEL="jplu/tf-flaubert-base-cased"
 #MODEL="jplu/tf-flaubert-large-cased"
@@ -51,33 +50,27 @@ MODEL="jplu/tf-xlm-roberta-large"
 #MODEL="TurkuNLP/bert-base-finnish-cased-v1"
 #MODEL="KB/bert-base-swedish-cased"
 BS=7
-EPOCHS=3
-LR=1e-5
+BGrate=1.0
 
-#for i in 1 2 3 4 5; do
-#for EPOCHS in 3; do
-#for LR in 8e-6 7e-6; do
-#echo "Settings: src=$SRC trg=$TRG model=$MODEL lr=$LR epochs=$EPOCHS batch_size=$BS"
-#echo "job=$SLURM_JOBID src=$SRC trg=$TRG model=$MODEL lr=$LR epochs=$EPOCHS batch_size=$BS" >> logs/experiments.log
-echo "Settings: src=$SRC trg=$TRG model_file=$3"
-
-srun python train_load.py \
-  --load_model $3 \
+#for i in $4; do
+#for EPOCHS in $3; do
+#for LR in $2; do
+echo "Settings: src=$SRC trg=$TRG bg=$BG model=$MODEL lr=$LR epochs=$EPOCHS batch_size=$BS"
+#echo "job=$SLURM_JOBID src=$SRC trg=$TRG bg=$BG model=$MODEL lr=$LR epochs=$EPOCHS batch_size=$BS bg_rate=$BGrate comment=saveFull"  >> logs/experiments.log
+srun python train.py \
   --model_name $MODEL \
-  --train $TRAIN_DIR/train.tsv \
+  --load_weights "$2" \
   --dev $DEV_DIR/dev.tsv \
   --test $DEV_DIR/test.tsv \
+  --bg_train "$BG_FILES" \
+  --bg_sample_rate $BGrate \
   --input_format tsv \
-  --lr $LR \
+  --threshold 0.5 \
   --seq_len 512 \
-  --epochs $EPOCHS \
   --batch_size $BS \
-  --log_file logs/testx_xlmrL_$SRC-$TRG.tsv
-#  --output_file $OUTPUT_DIR/model_xlmrL_$SRC-$TRG.h5 \
-#  --multiclass
-#--output_file $OUTPUT_DIR/model.h5 \
-#  --load_model $OUTPUT_DIR/model_nblocks3-ep10-2.h5 \
-#--load_model $OUTPUT_DIR/model.h5 \
+  --save_predictions "$OUTPUT_DIR/pred_$MODEL_ALIAS-0.5_$BG $SRC-$TRG-$i-$LR"
+
+
 #done
 #done
 #done
